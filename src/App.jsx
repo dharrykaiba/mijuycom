5be45e5b52367502d85faf0a8c525032890e21d4
Dharry Montoya <dharrykaiba@gmail.com>
2023-12-03 04:41:21 -0500

    home pages actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
 
       <header className="App-header">
-
+Proximamente...
         <img className=".App-logo" src='assets/logotipo_blanco.png' alt='' ></img>
       </header>
     </div>
