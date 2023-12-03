1b1a491c57d6de194de8847a11c5e581076be150
Dharry Montoya <dharrykaiba@gmail.com>
2023-12-03 03:54:10 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+
+        <img className=".App-logo" src='assets/logotipo_blanco.png' alt='' ></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
