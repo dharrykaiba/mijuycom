3a992883ccfb581e305517b550013e2d07dee30c
Dharry Montoya <dharrykaiba@gmail.com>
2024-12-07 13:15:58 -0500

    cambio horario cerrado

---
+++ src/components/Intro2/Intro2.jsx
@@ -0,0 +1,12 @@
+//src/components/Intro/Intro.jsx
+import "./Intro2.scss";
+
+export default function Intro2() {
+  return (
+    <div className="intro2" id="intro2">
+      <div className="centeredImage">
+        <img src="assets/web-HORAROP-NAVIDAD-web.png" alt="Logo de Mijuy" />
+      </div>
+    </div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,14 @@ import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 //import Construction from "./components/Construction/Construction";
-import Intro from "./components/Intro/Intro";
-import Carta from "./components/Carta/Carta";
-import C_Alitas from "./components/C_Alitas/C_Alitas";
-import C_Combos from "./components/C_Combos/C_Combos";
-import C_Burguer from "./components/C_Burguer/C_Burguer";
-import C_Otro from "./components/C_Otro/C_Otro";
-import C_Promos from "./components/C_Promos/C_Promos";
+//import Intro from "./components/Intro/Intro";
+import Intro2 from "./components/Intro2/Intro2";
+//import Carta from "./components/Carta/Carta";
+//import C_Alitas from "./components/C_Alitas/C_Alitas";
+//import C_Combos from "./components/C_Combos/C_Combos";
+//import C_Burguer from "./components/C_Burguer/C_Burguer";
+//import C_Otro from "./components/C_Otro/C_Otro";
+//import C_Promos from "./components/C_Promos/C_Promos";
 //import C_Pizzas from "./components/C_Pizzas/C_Pizzas";
 
 export default function Rutas() {
@@ -21,13 +22,13 @@ export default function Rutas() {
         element={
           <div className="sections">
             {/*<Construction />*/}
-            <Intro />
+            <Intro2 />
             {/*<C_Promos />*/}
-            <C_Combos />
+            {/*<C_Combos />
             <Carta />
             <C_Alitas />
             <C_Burguer />
-            <C_Otro />
+            <C_Otro />*/}
           </div>
         }
       />
